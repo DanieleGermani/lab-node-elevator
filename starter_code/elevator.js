c2ddf1060c44ec7aa31268913f31e8d28ba0038c
Daniele <daniele.germani.85@gmail.com>
2017-07-03 21:27:29 +0200

    Iteration 3 ok

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,70 @@
+/*jshint esversion: 6 */
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "Up";
+    this.waitingList = [];
+    this.passengers = [];
+    this.destinations = [];
+    this.servedPassengers = [];
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.intervalID = setInterval(() => this.update(), 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalID);
+  }
+
+  update() {
+    this.log();
+    this.floorDown();
+  }
+
+  _passengersEnter(indexOf) {
+    this.passengers.push(this.requests[indexOf]);
+    this.requests.splice(indexOf, 1);
+  }
+
+  _passengersLeave() {
+    this.servedPassengers.push(this.passengers[indexOf]);
+    this.passengers.splice(indexOf, 1);
+  }
+
+  floorUp() {
+    this.floor++;
+    if (this.floor > this.MAXFLOOR) {
+      this.floor = this.MAXFLOOR;
+      this.direction = "Up";
+    }
+
+  }
+  floorDown() {
+    this.floor--;
+    if (this.floor < 0) {
+      this.floor = 0;
+      this.direction = "Down";
+    }
+
+  }
+  call(person) {
+    this.requests.push(person);
+
+  }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+    console.log("Personas en espera:");
+    console.log(this.requests);
+    console.log(" has enter the elevator:");
+    console.log(this.passengers);
+    console.log("Los pasajeros depositados en destino son: ");
+    console.log(this.servedPassengers + "\n");
+  }
 }
+
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,12 @@
+/*jshint esversion: 6 */
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+const victor = new Person("Victor", 2, 9, "Up");
+const juan = new Person("Juan", 8, 4, "Down");
+const daniele = new Person("Daniele", 0, 8, "Up");
+const fernanda = new Person("Fernanda", 5, 1, "Down");
+const claudia = new Person("Claudia", 3, 6, "Up");
+
+let elevator = new Elevator();
+elevator.start();
+elevator.call(victor);
